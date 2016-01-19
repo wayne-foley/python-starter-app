116d03b92b7dda6e1a8fb42876306df19a4aab34
Billy Tat <billy.tat@hpe.com>
2016-01-19 11:30:24 -0800

    Replace hardcoded links and app name

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,78 +1,88 @@
-
-var vcap = "";
-
-
 var services = ["filesystem", "harbor", "memcached", "mongodb", "mysql", "postgresql", "rabbitmq3", "mssql2014"];
 var serviceEnabled = [0,0,0,0,0,0,0,0];
 
-
 $(document).ready(function() {
-	$(".collapsediv").hide();
+    $(".collapsediv").hide();
 	
-	$.get( "/vcap_services", function( data ) {
-		if(data){
-			var obj = $.parseJSON(data);
-		}
-		else{
-			var obj = {};
-		}
-		services.forEach(function(value, index, array) {
-			if(obj && obj[value] != null) {
-				
-				$("#als-service-buttons").append("<button id='"+value+"' class='btn btn-success togglebtn' type='button'>"+value+"</button>");
-	            $("#"+value+"_div").find("div.off").hide();
-	            $("#"+value+"_div").find("div.on").append("<pre>"+JSON.stringify(obj[value][0],null, "\t")+"</pre>");
+    $.get("/vcap_services", function(data) {
+        if (data) {
+            var obj = $.parseJSON(data);
+        }
+        else {
+            var obj = {};
+        }
 
-				console.log(value + ' found');
-				serviceObjs = obj[value];
-				
-				for ( i = 0; i < serviceObjs.length; i++) {
-					serviceEnabled[index] = 1;
-					label = serviceObjs[i]['label'];
-					console.log(label);
-					name = serviceObjs[i]['name'];
-					console.log(name);
-					creds = serviceObjs[i]['credentials'];
-				}
-				
-			}
-			else {
-				console.log(value + " not found");
-				$("#als-service-buttons").append("<button id='"+value+"' class='btn btn-default togglebtn' type='button'>"+value+"</button>");
-	            $("#"+value+"_div").find("div.on").hide();
-			}
-		});
+        services.forEach(function(value, index, array) {
+            if (obj && obj[value] != null) {
+                $("#als-service-buttons").append("<button id='" + value + "' class='btn btn-success togglebtn' type='button'>" + value + "</button>");
+	        $("#" + value + "_div").find("div.off").hide();
+	        $("#" + value + "_div").find("div.on").append("<pre>" + JSON.stringify(obj[value][0],null, "\t") + "</pre>");
 
-		 //Populate user-provided services
-		 console.log(obj);
-		 console.log(obj['user-provided']);
-        if(obj['user-provided'] != null) {
+                console.log(value + " found");
+                serviceObjs = obj[value];
+
+                for (i = 0; i < serviceObjs.length; i++) {
+                    serviceEnabled[index] = 1;
+                    label = serviceObjs[i]['label'];
+                    console.log(label);
+                    name = serviceObjs[i]['name'];
+                    console.log(name);
+                    creds = serviceObjs[i]['credentials'];
+                }
+            }
+            else {
+                console.log(value + " not found");
+                $("#als-service-buttons").append("<button id='" + value + "' class='btn btn-default togglebtn' type='button'>" + value + "</button>");
+	        $("#" + value + "_div").find("div.on").hide();
+            }
+        });
+
+        // Populate user-provided services
+        console.log(obj);
+        console.log(obj['user-provided']);
+
+        if (obj['user-provided'] != null) {
             //var user_provided = obj['user_provided'];
             //console.log(user_provided);
             //console.log(obj['user_provided']);
 
-            obj['user-provided'].forEach( function(v,i) {
+            obj['user-provided'].forEach(function(v,i) {
                 upName = v['name'];
-                $("#userprovided-service-buttons").append("<button id='"+upName+"' class='btn btn-success togglebtn udbtn' type='button'>"+upName+"</button>");
-                $("#userprovided_divs").append("<div class='collapsediv well on' id='"+upName+"_div'></div>");
-                $("#"+upName+"_div").hide();
-                $("#"+upName+"_div").append("<pre>"+JSON.stringify(v,null, "\t")+"</pre>");
-
+                $("#userprovided-service-buttons").append("<button id='" + upName + "' class='btn btn-success togglebtn udbtn' type='button'>" + upName + "</button>");
+                $("#userprovided_divs").append("<div class='collapsediv well on' id='" + upName + "_div'></div>");
+                $("#" + upName + "_div").hide();
+                $("#" + upName + "_div").append("<pre>"+JSON.stringify(v,null, "\t") + "</pre>");
             });
         }
-        else{
-        	$("#userprovided-services-howto_div").show();
+        else {
+            $("#userprovided-services-howto_div").show();
         }
 
-        //collapsed_div behavior for standard services
-  		$('.togglebtn').bind('click', function () {
-        	$(".collapsediv").hide();
-        	var target_id = this.id;
-        	$("#" + target_id + "_div").slideDown();
+        // collapsed_div behavior for standard services
+        $('.togglebtn').bind('click', function() {
+            $(".collapsediv").hide();
+            var target_id = this.id;
+            $("#" + target_id + "_div").slideDown();
+        });
+    });
 
-    	});
+    // Second section for VCAP Applications
+    $.get("/vcap_application", function(data) {
+        var app_name = "some-error";
 
-		});
+        if (data) {
+            app_name = $.parseJSON(data)["application_name"];
+        }
 
-	 
+        $(".app-name").text(app_name);
+    });
+
+    $.get("/links", function(data) {
+        var parsedData = $.parseJSON(data);
+        if (data) {
+            // Will be great if we can also do "ifs" for individual attribute in the data object before assigning.
+            $("a[class='cf-mgmt']").attr('href', parsedData["CF_MGMT_TOOL"]);
+            $("a[class='user-provided-service']").attr('href', parsedData["USER_SERVICE"]);
+        }
+    });
 });
